510549b05db1b9992f860e735ab84c967cb6b948
Sarah Camargo <sarahcamargo00@outlook.com>
2025-03-20 21:52:03 -0300

    feat: Pagina inicial imc

---
+++ imc/app/src/main/java/com/ifsc/imc/MainActivity.java
@@ -0,0 +1,29 @@
+package com.ifsc.imc;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(v -> {
+        });
+
+    }
+}
