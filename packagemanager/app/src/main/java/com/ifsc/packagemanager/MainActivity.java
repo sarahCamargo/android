efc90f2f61e0bd068cff160f67d735df65677167
Sarah Camargo <sarahcamargo00@outlook.com>
2025-06-12 20:23:18 -0300

    feat: list com apps

--- packagemanager/app/src/main/java/com/ifsc/packagemanager/MainActivity.java
+++ packagemanager/app/src/main/java/com/ifsc/packagemanager/MainActivity.java
@@ -1,8 +1,12 @@
 package com.ifsc.packagemanager;
 
+import android.content.Intent;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -21,23 +25,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lv = findViewById(R.id.listview);
         pm = getPackageManager();
 
-        List<ApplicationInfo> appsInfo = pm.getInstalledApplications(PackageManager.MATCH_ALL);
+        List<ApplicationInfo> appsInfo = new ArrayList<>();
 
-        List<ApplicationInfo> appsFiltrados = new ArrayList<>();
+        Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
 
-        for (ApplicationInfo appInfo : appsInfo) {
-            if ((appInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0) {
-                appsFiltrados.add(appInfo);
-            }
-        }
+        List<ResolveInfo> resolveInfos = pm.queryIntentActivities(intent, 0);
 
-        AppAdapter adapter = new AppAdapter(this, R.layout.item_app, appsFiltrados);
+        resolveInfos.forEach(resolveInfo -> {
+            appsInfo.add(resolveInfo.activityInfo.applicationInfo);
+        });
 
-        lv = findViewById(R.id.listview);
-
+        AppAdapter adapter = new AppAdapter(this, R.layout.item_app, appsInfo);
         lv.setAdapter(adapter);
 
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ApplicationInfo appClicked = (ApplicationInfo) parent.getItemAtPosition(position);
+
+                Intent i = pm.getLaunchIntentForPackage(appClicked.packageName);
+
+                //caso nao tenha so avisa o usuario
+                
+                startActivity(i);
+            }
+        });
     }
 }
